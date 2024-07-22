5ba1b6190a5b6d17ba74abc6b4c8651caa39ca20
HeyItsDavis <HeyItsDavis@users.noreply.github.com>
2024-07-21 23:47:37 -0700

    Fixed issue with prettier, and made change to demo page

---
+++ .eslintrc.mjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off', // New JSX transform doesn't require React in scope
+    'react/jsx-uses-react': 'off', // New JSX transform doesn't require React in scope
+  },
+};
